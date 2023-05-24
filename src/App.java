152a0e86de042580d24721762667ecd12f9093bd
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-05-24 10:38:52 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Silahkan tonton anime Oshi No Ko. The best");
     }
 }
